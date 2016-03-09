7a4f967811fa9cc3780d0ddbcadf2f88c5186caf
yqpan1991 <shifengzhongdexiao@163.com>
2016-03-10 07:48:08 +0800

    yqpan: import ExtendableRecyclerview

---
+++ extendablerecyclerview/src/main/java/com/edus/view/LoadingViewHolder.java
@@ -0,0 +1,13 @@
+package com.edus.view;
+
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/1/13.
+ */
+public class LoadingViewHolder<T> extends DmBaseViewHolder<T> {
+
+    public LoadingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
