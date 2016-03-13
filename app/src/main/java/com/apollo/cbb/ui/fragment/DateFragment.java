35fa76ec69bffc3fc1098ebc661a1259cfcffceb
yqpan1991 <shifengzhongdexiao@163.com>
2016-03-13 20:41:22 +0800

    yqpan:   约会分为 约会+我发出的约会

--- app/src/main/java/com/apollo/cbb/ui/fragment/DateFragment.java
+++ app/src/main/java/com/apollo/cbb/ui/fragment/DateFragment.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
@@ -42,14 +47,19 @@ import java.util.List;
 /**
  * Created by Panda on 2016/3/6.
  */
-public class DateFragment extends BaseFragment implements View.OnClickListener, EsUserManager.OnUserLogOperationListener {
+public class DateFragment extends BaseFragment implements View.OnClickListener{
 
-    private DmRecyclerViewWrapper mDrvwContent;
-    private RecommendAdapter mRecommendAdapter;
-    private TextView mTvEmpty;
-    private TextView mTvAdd;
+    private static final int INDEX_COMMON = 0;
+    private static final int INDEX_MIME = 1;
+    private static final int INDEX_COUNT = 2;
 
-    public static final int REQUEST_CODE_ADD_DATE = 2000;
+    private static final int INDEX_COMMON_TITLE = R.string.title_date_common;
+    private static final int INDEX_MIME_TITLE = R.string.title_date_mime;
+
+    private TabLayout mTlTitle;
+    private ViewPager mVpContent;
+    private MyPagerAdapter mPagerAdaper;
+
 
     @Nullable
     @Override
@@ -60,200 +70,67 @@ public class DateFragment extends BaseFragment implements View.OnClickListener, 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initTitle(view);
-        mDrvwContent = (DmRecyclerViewWrapper) view.findViewById(R.id.drvw_content);
-        mTvEmpty = (TextView) view.findViewById(R.id.tv_empty);
-        mTvEmpty.setOnClickListener(this);
-        mTvEmpty.setVisibility(View.INVISIBLE);
-    }
+        mTlTitle = (TabLayout) view.findViewById(R.id.tl_title);
+        mVpContent = (ViewPager) view.findViewById(R.id.vp_content);
+        mTlTitle.setTabMode(TabLayout.MODE_FIXED);
 
-    private void initTitle(View rootView) {
-        rootView.findViewById(R.id.back).setVisibility(View.GONE);
-        ((TextView)rootView.findViewById(R.id.center_title)).setText(R.string.nav_date);
-        mTvAdd = (TextView) rootView.findViewById(R.id.tv_operation);
-        mTvAdd.setVisibility(View.VISIBLE);
-        mTvAdd.setOnClickListener(this);
+        mPagerAdaper = new MyPagerAdapter(getChildFragmentManager());
+        mVpContent.setOffscreenPageLimit(INDEX_COUNT);
+        mVpContent.setAdapter(mPagerAdaper);
+        mTlTitle.setupWithViewPager(mVpContent);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mDrvwContent.setLayoutManager(new LinearLayoutManager(this.getActivity(), LinearLayoutManager.VERTICAL, false));
-        mDrvwContent.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
-        mRecommendAdapter = new RecommendAdapter(getActivity());
-        mDrvwContent.setAdapter(mRecommendAdapter);
-        mDrvwContent.enableLoadMore(false);
-        mDrvwContent.enableRefresh(false);
-        mDrvwContent.setOnRefreshListener(mOnRefreshListener);
-        loadData();
-        EsUserManager.getInstance().registerOnUserLogOperationListener(this);
     }
-
-    private SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
-        @Override
-        public void onRefresh() {
-            loadData();
-        }
-    };
-
-    private void setDataResult(List<RecommendInfo> recommendInfos){
-        mRecommendAdapter.setDataList(recommendInfos);
-        if(recommendInfos != null && !recommendInfos.isEmpty()){
-            mTvEmpty.setVisibility(View.INVISIBLE);
-        }else{
-            mTvEmpty.setText(R.string.data_result_empty);
-            mTvEmpty.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void loadData() {
-        if(!EsUserManager.getInstance().hasLogIn()){
-            mTvEmpty.setVisibility(View.VISIBLE);
-            mTvEmpty.setText(R.string.data_result_unlogin);
-            return;
-        }
-        UserInfo userInfo = EsUserManager.getInstance().getUserInfo();
-        EsApiHelper.fetchRecommendList(userInfo.getUserId(), RecommendInfo.RECOMMEND_TYPE_DATE, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                Log.e("DaylyRecommendFragment", s);
-                try {
-                    String results = (new JSONObject(s)).getJSONArray(EsApiKeys.KEY_RESPONSE_RESULTS).toString();
-                    List<RecommendInfo> recommendInfos = JSON.parseArray(results, RecommendInfo.class);
-                    mDrvwContent.enableRefresh(true);
-                    mDrvwContent.setRefreshing(false);
-                    setDataResult(recommendInfos);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                mDrvwContent.enableRefresh(true);
-                mDrvwContent.setRefreshing(false);
-                Log.e("DaylyRecommendFragment", volleyError.toString());
-                mTvEmpty.setVisibility(View.VISIBLE);
-                mTvEmpty.setText(R.string.data_result_error);
-                Snackbar.make(mDrvwContent, volleyError.toString(), Snackbar.LENGTH_LONG).setAction("retry", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mDrvwContent.setRefreshing(true);
-                    }
-                }).show();
-            }
-        });
-    }
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        EsUserManager.getInstance().unregisterOnUserLogOperationListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.tv_empty:
-                handleEmptyClicked();
-                break;
-            case R.id.tv_operation:
-                handleAdd();
-                break;
-        }
     }
 
-    private void handleAdd() {
-        if(EsUserManager.getInstance().hasLogIn()){
-            Intent intent = new Intent(getActivity(), CanDateListActivity.class);
-            startActivityForResult(intent, REQUEST_CODE_ADD_DATE);
-        }else{
-            Intent intent = new Intent(getActivity(), LoginActivity.class);
-            startActivity(intent);
-        }
-    }
+    private class MyPagerAdapter extends FragmentPagerAdapter {
 
-    private void handleEmptyClicked() {
-        if(EsUserManager.getInstance().hasLogIn()){
-            loadData();
-        }else{
-            Intent intent = new Intent(getActivity(), LoginActivity.class);
-            startActivity(intent);
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case REQUEST_CODE_ADD_DATE:
-                handleDateAdded(resultCode, data);
-                break;
-        }
-    }
-
-    private void handleDateAdded(int resultCode, Intent data) {
-        if(resultCode == Activity.RESULT_OK){
-            Toast.makeText(getActivity(), "data.toString() = "+ data.toString(), Toast.LENGTH_SHORT).show();
-            RecommendInfo info = (RecommendInfo) data.getSerializableExtra(CanDateListActivity.EXTRA_ADD_DATE_RESULT);
-            if(info == null){
-                return;
+        @Override
+        public Fragment getItem(int position) {
+            BaseFragment fragment = null;
+            switch (position){
+                case INDEX_COMMON:
+                    fragment = new CommonDateFragment();
+                    break;
+                case INDEX_MIME:
+                    fragment = new MyDateFragment();
+                    break;
             }
-            showAddDateDialog(info);
+            return fragment;
         }
-    }
 
-    private void showAddDateDialog(final RecommendInfo info) {
-        AddDateDialog dialog = new AddDateDialog(getActivity(), info, new AddDateDialog.AddDateResultListener(){
+        @Override
+        public int getCount() {
+            return INDEX_COUNT;
+        }
 
-            @Override
-            public void onCancelClicked() {
-
+        @Override
+        public CharSequence getPageTitle(int position) {
+            int titleResid = 0;
+            switch (position){
+                case INDEX_COMMON:
+                    titleResid = INDEX_COMMON_TITLE;
+                    break;
+                case INDEX_MIME:
+                    titleResid = INDEX_MIME_TITLE;
+                    break;
             }
-
-            @Override
-            public void onOKClicked(String editInfo) {
-                uploadRecommendInfo(info, editInfo);
-            }
-        });
-        dialog.show();
-    }
-
-    private void uploadRecommendInfo(RecommendInfo recommendInfo, String recommendDateInfo) {
-        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage(getString(R.string.recommend_uploading));
-        progressDialog.show();
-        UserInfo userInfo = EsUserManager.getInstance().getUserInfo();
-        EsApiHelper.uploadRecommend(userInfo.getUserId(), EsApiConst.RECOMMEND_TYPE_DATE,  recommendInfo.storeName, recommendInfo.latitude, recommendInfo.longtitude, recommendDateInfo,
-                new Response.Listener<String>() {
-
-                    @Override
-                    public void onResponse(String s) {
-                        progressDialog.dismiss();
-                        mDrvwContent.setRefreshing(true);
-                        loadData();
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        progressDialog.dismiss();
-                        Toast.makeText(getActivity(), volleyError.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-    }
-
-    @Override
-    public void onUserLogin() {
-        loadData();
-    }
-
-    @Override
-    public void onUserLout() {
-        loadData();
+            return getActivity().getString(titleResid);
+        }
     }
 }
---
+++ app/src/main/java/com/apollo/cbb/ui/fragment/MyDateFragment.java
@@ -0,0 +1,267 @@
+package com.apollo.cbb.ui.fragment;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.apollo.cbb.R;
+import com.apollo.cbb.biz.net.api.EsApiConst;
+import com.apollo.cbb.biz.net.api.EsApiHelper;
+import com.apollo.cbb.biz.net.api.EsApiKeys;
+import com.apollo.cbb.biz.net.model.RecommendInfo;
+import com.apollo.cbb.biz.user.EsUserManager;
+import com.apollo.cbb.biz.user.UserInfo;
+import com.apollo.cbb.ui.activity.CanDateListActivity;
+import com.apollo.cbb.ui.activity.LoginActivity;
+import com.apollo.cbb.ui.adapter.RecommendAdapter;
+import com.apollo.cbb.ui.dialog.AddDateDialog;
+import com.edus.view.DmRecyclerViewWrapper;
+import com.edus.view.decoration.DividerItemDecoration;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+/**
+ * Created by Panda on 2016/3/6.
+ */
+public class MyDateFragment extends BaseFragment implements View.OnClickListener, EsUserManager.OnUserLogOperationListener {
+
+    private DmRecyclerViewWrapper mDrvwContent;
+    private RecommendAdapter mRecommendAdapter;
+    private TextView mTvEmpty;
+
+    private RelativeLayout mRlOperation;
+    private Button mBtAdd;
+
+    public static final int REQUEST_CODE_ADD_DATE = 2000;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.my_date_fragment_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRlOperation = (RelativeLayout) view.findViewById(R.id.rl_operation);
+        mBtAdd = (Button) view.findViewById(R.id.bt_add);
+        mBtAdd.setOnClickListener(this);
+        mDrvwContent = (DmRecyclerViewWrapper) view.findViewById(R.id.drvw_content);
+        mTvEmpty = (TextView) view.findViewById(R.id.tv_empty);
+        mTvEmpty.setOnClickListener(this);
+        mTvEmpty.setVisibility(View.INVISIBLE);
+    }
+
+    private void updateLoginUi() {
+        if(EsUserManager.getInstance().hasLogIn()){
+            mRlOperation.setVisibility(View.VISIBLE);
+        }else{
+            mRlOperation.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mDrvwContent.setLayoutManager(new LinearLayoutManager(this.getActivity(), LinearLayoutManager.VERTICAL, false));
+        mDrvwContent.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+        mRecommendAdapter = new RecommendAdapter(getActivity());
+        mDrvwContent.setAdapter(mRecommendAdapter);
+        mDrvwContent.enableLoadMore(false);
+        mDrvwContent.enableRefresh(false);
+        mDrvwContent.setOnRefreshListener(mOnRefreshListener);
+        updateLoginUi();
+        loadData();
+        EsUserManager.getInstance().registerOnUserLogOperationListener(this);
+    }
+
+    private SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            loadData();
+        }
+    };
+
+    private void setDataResult(List<RecommendInfo> recommendInfos){
+        mRecommendAdapter.setDataList(recommendInfos);
+        if(recommendInfos != null && !recommendInfos.isEmpty()){
+            mTvEmpty.setVisibility(View.INVISIBLE);
+        }else{
+            mTvEmpty.setText(R.string.data_result_empty);
+            mTvEmpty.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void loadData() {
+        if(!EsUserManager.getInstance().hasLogIn()){
+            mTvEmpty.setVisibility(View.VISIBLE);
+            mTvEmpty.setText(R.string.data_result_unlogin);
+            return;
+        }
+        UserInfo userInfo = EsUserManager.getInstance().getUserInfo();
+        EsApiHelper.fetchRecommendList(userInfo.getUserId(), RecommendInfo.RECOMMEND_TYPE_DATE, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Log.e("DaylyRecommendFragment", s);
+                try {
+                    String results = (new JSONObject(s)).getJSONArray(EsApiKeys.KEY_RESPONSE_RESULTS).toString();
+                    List<RecommendInfo> recommendInfos = JSON.parseArray(results, RecommendInfo.class);
+                    mDrvwContent.enableRefresh(true);
+                    mDrvwContent.setRefreshing(false);
+                    setDataResult(recommendInfos);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                mDrvwContent.enableRefresh(true);
+                mDrvwContent.setRefreshing(false);
+                Log.e("DaylyRecommendFragment", volleyError.toString());
+                mTvEmpty.setVisibility(View.VISIBLE);
+                mTvEmpty.setText(R.string.data_result_error);
+                Snackbar.make(mDrvwContent, volleyError.toString(), Snackbar.LENGTH_LONG).setAction("retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mDrvwContent.setRefreshing(true);
+                    }
+                }).show();
+            }
+        });
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        EsUserManager.getInstance().unregisterOnUserLogOperationListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_empty:
+                handleEmptyClicked();
+                break;
+            case R.id.bt_add:
+                handleAdd();
+                break;
+        }
+    }
+
+    private void handleAdd() {
+        if(EsUserManager.getInstance().hasLogIn()){
+            Intent intent = new Intent(getActivity(), CanDateListActivity.class);
+            startActivityForResult(intent, REQUEST_CODE_ADD_DATE);
+        }else{
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void handleEmptyClicked() {
+        if(EsUserManager.getInstance().hasLogIn()){
+            loadData();
+        }else{
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_CODE_ADD_DATE:
+                handleDateAdded(resultCode, data);
+                break;
+        }
+    }
+
+    private void handleDateAdded(int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK){
+            Toast.makeText(getActivity(), "data.toString() = "+ data.toString(), Toast.LENGTH_SHORT).show();
+            RecommendInfo info = (RecommendInfo) data.getSerializableExtra(CanDateListActivity.EXTRA_ADD_DATE_RESULT);
+            if(info == null){
+                return;
+            }
+            showAddDateDialog(info);
+        }
+    }
+
+    private void showAddDateDialog(final RecommendInfo info) {
+        AddDateDialog dialog = new AddDateDialog(getActivity(), info, new AddDateDialog.AddDateResultListener(){
+
+            @Override
+            public void onCancelClicked() {
+
+            }
+
+            @Override
+            public void onOKClicked(String editInfo) {
+                uploadRecommendInfo(info, editInfo);
+            }
+        });
+        dialog.show();
+    }
+
+    private void uploadRecommendInfo(RecommendInfo recommendInfo, String recommendDateInfo) {
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage(getString(R.string.recommend_uploading));
+        progressDialog.show();
+        UserInfo userInfo = EsUserManager.getInstance().getUserInfo();
+        EsApiHelper.uploadRecommend(userInfo.getUserId(), EsApiConst.RECOMMEND_TYPE_DATE,  recommendInfo.storeName, recommendInfo.latitude, recommendInfo.longtitude, recommendDateInfo,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String s) {
+                        progressDialog.dismiss();
+                        mDrvwContent.setRefreshing(true);
+                        loadData();
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getActivity(), volleyError.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    @Override
+    public void onUserLogin() {
+        updateLoginUi();
+        loadData();
+    }
+
+    @Override
+    public void onUserLout() {
+        updateLoginUi();
+        loadData();
+    }
+}
